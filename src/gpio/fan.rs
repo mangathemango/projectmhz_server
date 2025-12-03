a214fd663d22dae45b2b24160be139e1e1ac0235
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-12-03 14:48:58 +0800

    fix: wrong return message

--- src/gpio/fan.rs
+++ src/gpio/fan.rs
@@ -12,6 +12,6 @@ pub async fn fan_off() -> &'static str {
     let mut pin = OutputPinRole::get_pin(&OutputPinRole::Fan);
     println!("Turning fan off...");
     pin.set_low();
-    
-    "Fan has been requested to turn on"
+
+    "Fan has been requested to turn off"
 }
