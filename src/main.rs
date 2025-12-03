889a2dcd65f9dc2562e2168e704f109d36aadc60
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-12-03 13:49:10 +0800

    fix: change listener address in main

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() {
     let app = Router::new()
         .route("/buzz",   post(buzz));
 
-    let addr = "0.0.0.0:3000";
+    let addr = "0.0.0.0:4000";
     println!("Listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
